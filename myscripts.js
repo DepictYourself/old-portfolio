ba63ad0d29882f75d9cab27bdc06dabfa5101cc9
Ágoston Tóth <DepictYourself@users.noreply.github.com>
2017-03-17 17:34:50 +0100

    Added scrolling icons with jQuery
    scoll script is located in myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,17 @@
-window.onload = function currentYear () {
+window.onload = function currentYear() {
     var currentyear = new Date().getFullYear();
     document.getElementById("copyright").innerHTML = currentyear;
     console.log("function currentYear() run succesfully");
 }
+
+$(document).ready(function() {
+    
+    $('.slide-section').click(function(e) {
+        var linkhref = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(linkhref).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+    
+});
